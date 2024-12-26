de3ed891910cb72357ae4a498123228f048700a5
Imtanan_Aziz_Toor <imtanan@smash.cloud>
2024-12-26 12:35:48 +0500

    Dev branch initialzied

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main>
-        <h1> Press 360</h1>
-        <p>
-          Press 360 is a platform for creating and sharing 360° videos.
-        </p>
-      </main>
-    </div>
+    <main>
+      <h1> Press 360</h1>
+      <p>
+        Press 360 is a platform for creating and sharing 360° videos.
+      </p>
+    </main>
   );
 }
 
