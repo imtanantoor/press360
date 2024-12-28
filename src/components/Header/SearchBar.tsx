51a9c76b3e2529ce34b6a49af45e071939d1b090
Imtanan_Aziz_Toor <imtanan@smash.cloud>
2024-12-28 18:36:54 +0500

    w.i.p

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import searchIcon from '../../assets/icons/searchIcon.svg'
 import useDebounce from "../../hooks/debounce";
 
 function SearchBar() {
@@ -13,12 +14,15 @@ function SearchBar() {
   }
 
   return (
-    <input
-      type="text"
-      placeholder={"Search"}
-      className="search-bar"
-      onChange={handleChange}
-    />
+    <div className="search-bar-container">
+      <input
+        type="text"
+        placeholder={"Search"}
+        className="search-bar"
+        onChange={handleChange}
+      />
+      <img src={searchIcon} alt="search" className="search-icon" />
+    </div>
   );
 }
 
